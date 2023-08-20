ff04d11b8f98f04c64521eceaa0f446572741bf5
mdelvecchio <mdelvecchio3119@gmail.com>
2023-08-20 15:19:44 -0400

    setting up auth on the backend

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// Importing Middleware
 const cors = require("cors");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //////////////////////
 // Database Connection
@@ -25,6 +27,14 @@ mongoose.connection
 //////////////////////
 // Models
 //////////////////////
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+})
+
+const User = mongoose.model("User", UserSchema)
+
 const AlbumsSchema = new mongoose.Schema({
     albumName: String,
     artist: String,
@@ -36,9 +46,28 @@ const AlbumsSchema = new mongoose.Schema({
 const Albums = mongoose.model("Albums", AlbumsSchema)
 
 //////////////////////
+// Auth Middleware
+//////////////////////
+
+const authCheck = async (req, res, next) => {
+    if(req.cookies.token) {
+        const payload = await jwt.verify(req.cookies.token, process.env.SECRET)
+        req.payload = payload;
+        next();
+    } else {
+        res.status(400).json({error: "You are not authorized"})
+    }
+}
+
+//////////////////////
 // Declare Middleware
 //////////////////////
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://personal-jukebox-nxb4.onrender.com",
+        credentials: true,
+    }));
+app.use(cookieParser())
 app.use(morgan("dev"));
 app.use(express.json());
 
@@ -48,7 +77,7 @@ app.use(express.json());
 // INDUCES - Index, New, Delete, Update, Create, Edit, Show
 
 //Index// 
-app.get("/albums", async (req,res) => {
+app.get("/albums", authCheck, async (req,res) => {
     try{
         const album = await Albums.find({})
         res.json(album)
@@ -58,7 +87,7 @@ app.get("/albums", async (req,res) => {
 });
 
 // DESTROY
-app.delete("/albums/:id", async (req, res) => {
+app.delete("/albums/:id", authCheck, async (req, res) => {
     try{
         const album = await Albums.findByIdAndDelete(req.params.id)
     res.status(204).json(album)
@@ -68,7 +97,7 @@ app.delete("/albums/:id", async (req, res) => {
 });
 
 // UPDATE
-app.put("/albums/:id", async (req, res) => {
+app.put("/albums/:id", authCheck, async (req, res) => {
     try {
         const album = await Albums.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
@@ -80,7 +109,7 @@ app.put("/albums/:id", async (req, res) => {
 });
 
 //CREATE//
-app.post("/albums", async (req,res) => {
+app.post("/albums", authCheck, async (req,res) => {
     try{
         // create album
         const album = await Albums.create(req.body)
@@ -91,7 +120,7 @@ app.post("/albums", async (req,res) => {
 })
 
 //Show//
-app.get("/albums/:id", async (req,res) => {
+app.get("/albums/:id", authCheck, async (req,res) => {
     try{
         // get a album from the database
         const album = await Albums.findById(req.params.id)
@@ -102,8 +131,59 @@ app.get("/albums/:id", async (req,res) => {
     }
 })
 
+// test route
 app.get("/", (req, res) => {
     res.send("Hello world!")
+})
+
+///////////////////////////
+// Auth Routes
+///////////////////////////
+
+// Signup - Post
+app.post("/signup", async (req, res) => {
+    try {
+        let {username, password} = req.body
+        password = await bcrypt.hash(password, await bcrypt.genSalt(15))
+        const user = await User.create({username, password})
+        res.json(user)
+    } catch(error) {
+        res.status(400).json({error})
+    }
+})
+
+// Login - Post
+app.post("/login", async (req, res) => {
+    try {
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        if (!user) {
+            throw new Error("No user with that username was found!")
+        }
+        const passwordCheck = await bcrypt.compare(password, user.password)
+
+        if (!passwordCheck) {
+            throw new Error("Password does not match! Try again.")
+        }
+        const token = jwt.sign({username: user.username}, process.env.SECRET)
+        res.cookie("token", token, {
+            httpOnly: true,
+            path: "/",
+            domain: "render.com",
+            secure: "true",
+            sameSite: "none",
+            maxAge: 3600000,
+        });
+        res.json({message: "Login successful!"})
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+// Logout - Get
+app.get("/logout", (req, res) => {
+    res.clearCookie("token");
+    res.json({message: "You have been logged out successfully!"})
 })
 
 ///////////////////////////
